472ff2b9020731ed21fe15090bd8dd05e3fcbd53
Gabitoto <gabirondolucas@gmail.com>
2025-01-05 14:52:34 -0300

    agrego guia 2

---
+++ Guia de problemas n2/Guia_n2.py
@@ -0,0 +1,4 @@
+"""Implementar una clase que modele a un cuadrado. El único atributo que posee es su lado.
+ Dotar de operaciones al Cuadrado para ingresar el lado, mostrarlo, calcular y mostrar el
+ área, calcular y mostrar el perímetro.
+ Evaluar los métodos donde es necesario hacer una validación de datos e implementar."""
