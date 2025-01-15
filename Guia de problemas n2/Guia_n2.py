4507deeb233bdecf6c5b82945f44f0fa95c3033e
Gabitoto <gabirondolucas@gmail.com>
2025-01-14 23:45:16 -0300

    agrego actividad numero 2

--- Guia de problemas n2/Guia_n2.py
+++ Guia de problemas n2/Guia_n2.py
@@ -3,6 +3,7 @@
  área, calcular y mostrar el perímetro.
  Evaluar los métodos donde es necesario hacer una validación de datos e implementar."""
 
+from re import X
 from tabulate import tabulate
 
 class Cuadrado:
@@ -38,3 +39,49 @@ if __name__ == "__main__":
     print(cuadrado.mostrar())
     print(f"\nÁrea del cuadrado: {cuadrado.area()}")
     print(f"Perímetro del cuadrado: {cuadrado.perimetro()}")
+
+"""Implementar una clase que modele un punto compuesto por dos coordenadas ‘x’ y ‘y’. El
+ punto debe contar con operaciones para: proporcionar un valor al punto al momento de
+ instanciarlo, proporcionar valores a sus atributos y para retornarlos.
+ Evaluar los métodos donde es necesario hacer una validación de datos e implementar."""
+ 
+class Punto:
+    def __init__(self, x=0, y=0):
+        self.set_x(x)
+        self.set_y(y)
+
+    def set_x(self, x):
+        if isinstance(x, (int, float)):
+            self.x = x
+        else:
+            raise ValueError("La coordenada x debe ser un número.")
+
+    def set_y(self, y):
+        if isinstance(y, (int, float)):
+            self.y = y
+        else:
+            raise ValueError("La coordenada y debe ser un número.")
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
+
+    def ingresar(self):
+        while True:
+            try:
+                x = float(input("Agrega un número para la coordenada x: "))
+                y = float(input("Agrega un número para la coordenada y: "))
+                self.set_x(x)
+                self.set_y(y)
+                print(f"Las coordenadas son: ({self.x}, {self.y})")
+                break
+            except ValueError:
+                print("Por favor, ingresa números válidos.")
+
+if __name__ == "__main__":
+    puntoA = Punto()
+    puntoA.ingresar()
+    print(f"Coordenada x: {puntoA.get_x()}")
+    print(f"Coordenada y: {puntoA.get_y()}")
